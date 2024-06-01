59b158f5dcbd162ecc5d586bb7123ed56d77c4b8
isaac-svg <isaacsakyi009@gmail.com>
2024-06-01 19:15:18 +0000

    Fix typo

--- app/components/sections/home/whyweare.tsx
+++ app/components/sections/home/whyweare.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Glassmorphismcard from '../../shared/glassmorphismcard';
+import ObserverButtomUp from '@/lib/oberser';
+import HorizontalSlideEffect from '@/lib/horizontalslideinobserver';
 
 type Props = {};
 
@@ -12,27 +14,45 @@ const WhyWeAre = (props: Props) => {
       <div className="flex items-center gap-4 justify-center w-full flex-col  ">
         <div className="flex items-center gap-4 ">
           {' '}
-          <div className="bg-[#d2d1e9] w-8 h-1  " />{' '}
-          <h3 className="text-[#d2d1e9] text-bold uppercase">WHY WE ARE</h3>
+          <ObserverButtomUp>
+            <div className="bg-[#d2d1e9] w-8 h-[1px]  " />{' '}
+          </ObserverButtomUp>
+          <ObserverButtomUp>
+            <h3 className="text-[#d2d1e9] text-bold text-xl uppercase">
+              WHY WE ARE
+            </h3>
+          </ObserverButtomUp>
         </div>
         <div className="max-w-[800px]">
-          <h2 className="text-4xl text-center text-[#d2d1e9] font-extrabold  ">
-            We provide all kinds of logistics services
-          </h2>
+          <ObserverButtomUp>
+            <h2 className="text-4xl text-center text-[#d2d1e9] font-extrabold  ">
+              We provide all kinds of logistics services
+            </h2>
+          </ObserverButtomUp>
         </div>
         <div className="max-w-[800px]">
-          <p className="text-[#d2d1e9]">
-            Effortlessly monitor your parcels&apos; journeys with our intuitive
-            platform, ensuring seamless delivery every time.
-          </p>
+          <ObserverButtomUp>
+            <p className="text-[#d2d1e9]">
+              Effortlessly monitor your parcels&apos; journeys with our
+              intuitive platform, ensuring seamless delivery every time.
+            </p>
+          </ObserverButtomUp>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 auto-cols-fr justify-center items-start w-full ">
           {/* card */}
 
-          <Glassmorphismcard iconsrc="/assets/home/oceanfreight.svg" />
-          <Glassmorphismcard iconsrc="/assets/home/airfreight.svg" />
-          <Glassmorphismcard iconsrc="/assets/home/railfreight.svg" />
-          <Glassmorphismcard iconsrc="/assets/home/roadfreight.svg" />
+          <HorizontalSlideEffect>
+            <Glassmorphismcard iconsrc="/assets/home/oceanfreight.svg" />
+          </HorizontalSlideEffect>
+          <HorizontalSlideEffect>
+            <Glassmorphismcard iconsrc="/assets/home/airfreight.svg" />
+          </HorizontalSlideEffect>
+          <HorizontalSlideEffect>
+            <Glassmorphismcard iconsrc="/assets/home/railfreight.svg" />
+          </HorizontalSlideEffect>
+          <HorizontalSlideEffect>
+            <Glassmorphismcard iconsrc="/assets/home/roadfreight.svg" />
+          </HorizontalSlideEffect>
         </div>
       </div>
     </section>
